30b929e01c7887d44e2d2671dadc86b16aeb276e
Adrian Chiriac <52508424+chiriacadrian@users.noreply.github.com>
2020-05-12 12:57:33 +0200

    Updates

---
+++ precache-manifest.ceacd77de3556717ac76acd5ada30eef.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ea8c53ec51d6e3d6e663da963fbd91b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "10659b3eac46693d847e",
+    "url": "/portfolio/static/css/2.7151161e.chunk.css"
+  },
+  {
+    "revision": "adb3cbb798d21adbe116",
+    "url": "/portfolio/static/css/main.14248994.chunk.css"
+  },
+  {
+    "revision": "10659b3eac46693d847e",
+    "url": "/portfolio/static/js/2.8ffc6521.chunk.js"
+  },
+  {
+    "revision": "4275c1d12d633733c6fec0905104cb9a",
+    "url": "/portfolio/static/js/2.8ffc6521.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adb3cbb798d21adbe116",
+    "url": "/portfolio/static/js/main.9e5df859.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "4a6bd2d1fbb2a81e5d3c4be300ac4080",
+    "url": "/portfolio/static/media/converter.4a6bd2d1.jpg"
+  },
+  {
+    "revision": "5c4e16ceb77211d7b286e351e272328d",
+    "url": "/portfolio/static/media/css.5c4e16ce.png"
+  },
+  {
+    "revision": "46454f1dd7b3443344f31824a78a9640",
+    "url": "/portfolio/static/media/exams.46454f1d.jpg"
+  },
+  {
+    "revision": "036bf7c4f6b41f6714f72edd83d291d2",
+    "url": "/portfolio/static/media/git.036bf7c4.png"
+  },
+  {
+    "revision": "b74707056b0080fdb459c76e55eed763",
+    "url": "/portfolio/static/media/invision.b7470705.png"
+  },
+  {
+    "revision": "3b99a49d355fee2fc23d4f1c99da7701",
+    "url": "/portfolio/static/media/jquery.3b99a49d.png"
+  },
+  {
+    "revision": "288033f3dcb3b1b3365ae2e8a12aea70",
+    "url": "/portfolio/static/media/netflix.288033f3.jpg"
+  },
+  {
+    "revision": "effe1bf76222b70eda82213bc78b57ff",
+    "url": "/portfolio/static/media/photo.effe1bf7.jpg"
+  },
+  {
+    "revision": "d6bdcfb24f57bffcd8e8b695f75ef263",
+    "url": "/portfolio/static/media/react.d6bdcfb2.png"
+  },
+  {
+    "revision": "3286476a236eedf1dd65dbf99d28b033",
+    "url": "/portfolio/static/media/sass.3286476a.png"
+  },
+  {
+    "revision": "06f9e45948843330905ce0324f0b1f98",
+    "url": "/portfolio/static/media/sql.06f9e459.png"
+  },
+  {
+    "revision": "27bc4d3af4b5f08095d2e5a2a0a0edef",
+    "url": "/portfolio/static/media/viotrans.27bc4d3a.jpg"
+  }
+]);
