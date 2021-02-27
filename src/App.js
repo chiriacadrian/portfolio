22d863d70a2ad770ce640e846395349e0323253a
Adrian Chiriac <52508424+chiriacadrian@users.noreply.github.com>
2021-02-27 10:57:18 +0100

    Work section, added skills and certificates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { ProjectCard } from './components/ProjectCard';
+import { ProjectCard, WorkCard } from './components/ProjectCard';
 
 // Project photos and avatar
 import Photo from './images/photo.jpg';
@@ -25,35 +25,51 @@ import Photoshop from './images/icons/photoshop.png'
 import Reactjs from './images/icons/react.png'
 import Sass from './images/icons/sass.png'
 import Sql from './images/icons/sql.png'
+import AdobeXd from './images/icons/adobe xd.png'
+import ApiWebServices from './images/icons/api web services.png'
+import AspDotNetMVC from './images/icons/asp.net mvc.png'
+import AzureDevopsServer from './images/icons/azure devops server.png'
+import CSharp from './images/icons/CSharp.png'
+import Dart from './images/icons/dart.png'
+import Figma from './images/icons/figma.png'
+import Json from './images/icons/json.png'
+import MaterialUI from './images/icons/material ui.png'
+import Npm from './images/icons/npm.png'
+import Postman from './images/icons/postman.png'
+import VisualStudioCode from './images/icons/visual studio code.png'
+import VisualStudio from './images/icons/visual studio.png'
+
 
 // Contact icons
 import Github from './images/icons/github.png'
 import Linkedin from './images/icons/linkedin.png'
 import Call from './images/icons/call.png'
 import Mail from './images/icons/mail.png'
+import { Button } from 'shards-react';
 
 function App() {
   return (
     <div className="App" id="home">
       <header className="App-header">
-        <div id="navbar" className="container" style={{ margin: "0px", zIndex: "100"}}>
+        <div id="navbar" className="container" style={{ margin: "0px", zIndex: "2000" }}>
           <div id="navlinks">
             <a href="#home" >Home</a>
-            <a href="#about" >About</a>
+            {/* <a href="#about" >About</a> */}
+            <a href="#work" >Work</a>
             <a href="#projects" >Projects</a>
             <a href="#skills" >Skills</a>
             <a href="#contact" >Contact</a>
           </div>
         </div>
-        <div style={{zIndex: "50"}}>
+        <div style={{ zIndex: "50" }}>
           <img src={Photo} alt="Avatar" />
           <div style={hor_space_40px}></div>
           <h1 style={whiteHeading}>
             Chiriac Adrian
         </h1>
           <h2 style={whiteHeading}>
-            Developer, Designer, Music{'\u00A0'}Producer
-        </h2>
+            Full Stack Developer & Graphic Designer
+          </h2>
         </div>
 
         <div className="waveWrapper waveAnimation">
@@ -70,9 +86,42 @@ function App() {
         </div>
 
       </header>
-      <section id="about" className="container">
+      {/* <section id="about" className="container">
         <h3 id="h3">About</h3>
-        <p>I am passionate about development, music and graphics because in these fields I have the opportunity to express myself and to create something useful and beautiful at the same time. Putting art and technology together leads to the improvement of both, in my opinion. I love foreign languages, literature and philosophy because they are tools for studying man and life. In my spare time I play sports, study, create, that is work on some personal project, or I spend time with my friends and family.</p>
+        <p>I am passionate about development, music and graphics because in these fields I have the opportunity to express myself and to create something useful and beautiful at the same time. Putting art and technology together leads to the improvement of both, in my opinion. I love foreign languages, literature and philosophy because they are tools for studying man and life. In my spare time I play sports, study, create, that is work on some personal project, or I spend time with my friends and family. Where I've worked and what I've learned.</p>
+      </section> */}
+      <section id="work" className="container" >
+        <h3 id="h3">My Work Experience</h3>
+        <WorkCard 
+          title="Back End Developer | Generali Group"
+          subtitle="June 2020 - Present"
+          description="
+                        I'm currently working on developing the back end of a private mobile iOS app used by insurance agents to aid them in their daily operations and to offer a modern alternative for their superiors for knowing what's going on with regards to their teams at any level, at any time and for better managing their work load.
+                        More practically, I write SQL scripts, queries and stored procedures mostly for new features requested by our users but also for internal uses like maintanance jobs or for improving our work flow. I also manage the database in terms of creating, modifying and deleting tables.
+                        On the C# part, I develop web services in a MVC architecture. We use Entity to interact with the DB by calling our SP's. These API's are used by the FE to make requests to us for anything that has to do with data.
+                      " 
+          technologies="Tools and technologies used: Microsoft's SQL Server (RDBMS), SQL, Excel, Visual Studio, C# (OOP), ASP.NET MVC, Razor, HTML, CSS, TFS for version control (now known as Azure DevOps Server)"
+        />
+        <WorkCard 
+          title="Full Stack Developer | GisData SRL"
+          subtitle="September 2019 - June 2020"
+          description="
+                        Backend operations support for Python based automatic ticketing management systems. Used to do data extractions and data manipulation.
+                        I was made responsible for rebuilding an already existing mobile app called inforMAPP from the ground up utilizing Flutter, Google’s framework and UI toolkit for building natively compiled applications for mobile, web, and desktop from a single codebase, that is based on Dart, a programming language very similar to Javascript. The old version of the app is still available for both Android and iOS on their respective app stores.
+                      " 
+          technologies="Tools and technologies used: Microsoft's SQL Server Management (RDBMS), SQL, Excel, Dart, Flutter, JSON in asynchronous HTTP requests, GIT, SmartGit and some Amazon Web Services."
+        />
+        <WorkCard 
+          title="Web Developer & Designer | Vio Transgroup SRL"
+          subtitle="June 2019 - September 2019"
+          description="
+                        This was my first real work experience as a web developer and designer. I was requiered to create a static website to enable my client to have an online presence and also to showcase his company's strenghts.
+                        I was entirely responsible for the project and have successfully managed every step of the development process: from the initial design phase (wireframe, mock-up, logo design, website structure, colors pattern choice, etc.) to the actual development and final deployment via FTP on the client's server.
+                        I constantly kept in touch with my client every step of the way to ensure that we were heading in the right direction so that, at the end, we would both be happy about the result.
+                        The final product features a photo gallery, Google Maps iframes, an email contact form and is completely responsive. 
+                      " 
+          technologies="Tools and technologies used: Photoshop, HTML, CSS, Javascript, Bootstrap, PHP (for the form) and Filezilla."
+        />
       </section>
       <section id="projects" className="container">
         <h3 id="h3">My Projects</h3>
@@ -146,19 +195,125 @@ function App() {
       </section>
       <section id="skills" className="container">
         <h3 id="h3">My Skills</h3>
-        <img src={Html} alt="html" style={skillstyle} />
-        <img src={Css} alt="css" style={skillstyle} />
-        <img src={Sass} alt="sass" style={skillstyle} />
-        <img src={Javascript} alt="javascript" style={skillstyle} />
-        <img src={Jquery} alt="jquery" style={skillstyle} />
-        <img src={Bootstrap} alt="bootstrap" style={skillstyle} />
-        <img src={Reactjs} alt="reactjs" style={skillstyle} />
-        <img src={Flutter} alt="flutter" style={skillstyle} />
-        <img src={Sql} alt="sql" style={skillstyle} />
-        <img src={Git} alt="git" style={skillstyle} />
-        <img src={Invision} alt="invision" style={skillstyle} />
-        <img src={Photoshop} alt="photoshop" style={skillstyle} />
+        <div className="tooltip" style={skillStyle}>
+          <img src={Html} alt="html" id="html" style={skillStyle} style={{ maxWidth: "100%" }} />
+          <h5 className="tooltiptext">HTML</h5>
+        </div>
+        <div className="tooltip" style={skillStyle}>
+          <img src={Css} alt="css" id="css" style={skillStyle} style={{ maxWidth: "100%" }} />
+          <h5 className="tooltiptext">CSS</h5>
+        </div>
+        {/* <div className="tooltip" style={skillStyle}>
+          <img src={Sass} alt="sass" style={skillStyle} style={{ maxWidth: "100%" }} />
+          <h5 className="tooltiptext">SASS</h5>
+        </div> */}
+        <div className="tooltip" style={skillStyle}>
+          <img src={Javascript} alt="javascript" style={skillStyle} style={{ maxWidth: "100%" }} />
+          <h5 className="tooltiptext">Javascript</h5>
+        </div>
+        <div className="tooltip" style={skillStyle}>
+          <img src={Jquery} alt="jquery" style={skillStyle} style={{ maxWidth: "100%" }} />
+          <h5 className="tooltiptext">JQuery</h5>
+        </div>
+        <div className="tooltip" style={skillStyle}>
+          <img src={Bootstrap} alt="bootstrap" style={skillStyle} style={{ maxWidth: "100%" }} />
+          <h5 className="tooltiptext">Bootstrap</h5>
+        </div>
+        <div className="tooltip" style={skillStyle}>
+          <img src={Reactjs} alt="reactjs" style={skillStyle} style={{ maxWidth: "100%" }} />
+          <h5 className="tooltiptext">React.js / React Native</h5>
+        </div>
+
+
+
+        <div className="tooltip" style={skillStyle}>
+          <img src={Dart} alt="dart" id="dart" style={skillStyle} style={{ maxWidth: "100%" }} />
+          <h5 className="tooltiptext">Dart</h5>
+        </div>
+        <div className="tooltip" style={skillStyle}>
+          <img src={Flutter} alt="flutter" style={skillStyle} style={{ maxWidth: "100%" }} />
+          <h5 className="tooltiptext">Flutter</h5>
+        </div>
+        <div className="tooltip" style={skillStyle}>
+          <img src={VisualStudioCode} alt="visualstudiocode" id="visualstudiocode" style={skillStyle} style={{ maxWidth: "100%" }} />
+          <h5 className="tooltiptext">Visual Studio Code</h5>
+        </div>
+
+
+
+        <div className="tooltip" style={skillStyle}>
+          <img src={Invision} alt="invision" style={skillStyle} style={{ maxWidth: "100%" }} />
+          <h5 className="tooltiptext">InVision</h5>
+        </div>
+        <div className="tooltip" style={skillStyle}>
+          <img src={Photoshop} alt="photoshop" id="photoshop" style={skillStyle} style={{ maxWidth: "100%" }} />
+          <h5 className="tooltiptext">Photoshop</h5>
+        </div>
+        <div className="tooltip" style={skillStyle}>
+          <img src={AdobeXd} alt="adobexd" id="adobexd" style={skillStyle} style={{ maxWidth: "100%" }} />
+          <h5 className="tooltiptext">Adobe Xd</h5>
+        </div>
+        <div className="tooltip" style={skillStyle}>
+          <img src={Figma} alt="figma" id="figma" style={skillStyle} style={{ maxWidth: "100%" }} />
+          <h5 className="tooltiptext">Figma</h5>
+        </div>
+
+        
+
+        <div className="tooltip" style={skillStyle}>
+          <img src={Sql} alt="sql" style={skillStyle} style={{ maxWidth: "100%" }} />
+          <h5 className="tooltiptext">SQL</h5>
+        </div>
+        <div className="tooltip" style={skillStyle}>
+          <img src={CSharp} alt="csharp" id="csharp" style={skillStyle} style={{ maxWidth: "100%" }} />
+          <h5 className="tooltiptext">C#</h5>
+        </div>
+        <div className="tooltip" style={skillStyle}>
+          <img src={AspDotNetMVC} alt="aspdotnetmvc" id="aspdotnetmvc" style={skillStyle} style={{ maxWidth: "100%" }} />
+          <h5 className="tooltiptext">ASP.NET MVC</h5>
+        </div>
+        <div className="tooltip" style={skillStyle}>
+          <img src={AzureDevopsServer} alt="tfs" id="tfs" style={skillStyle} style={{ maxWidth: "100%" }} />
+          <h5 className="tooltiptext">TFS</h5>
+        </div>
+        <div className="tooltip" style={skillStyle}>
+          <img src={VisualStudio} alt="visualstudio" id="visualstudio" style={skillStyle} style={{ maxWidth: "100%" }} />
+          <h5 className="tooltiptext">Visual Studio</h5>
+        </div>
+
+
+
+        <div className="tooltip" style={skillStyle}>
+          <img src={Json} alt="json" id="json" style={skillStyle} style={{ maxWidth: "100%" }} />
+          <h5 className="tooltiptext">JSON</h5>
+        </div>
+        <div className="tooltip" style={skillStyle}>
+          <img src={MaterialUI} alt="materialui" id="materialui" style={skillStyle} style={{ maxWidth: "100%" }} />
+          <h5 className="tooltiptext">Material UI</h5>
+        </div>
+        <div className="tooltip" style={skillStyle}>
+          <img src={Npm} alt="npm" id="npm" style={skillStyle} style={{ maxWidth: "100%" }} />
+          <h5 className="tooltiptext">npm</h5>
+        </div>
+        <div className="tooltip" style={skillStyle}>
+          <img src={Git} alt="git" style={skillStyle} style={{ maxWidth: "100%" }} />
+          <h5 className="tooltiptext">GIT</h5>
+        </div>
+        <div className="tooltip" style={skillStyle}>
+          <img src={ApiWebServices} alt="apiwebservices" id="apiwebservices" style={skillStyle} style={{ maxWidth: "100%" }} />
+          <h5 className="tooltiptext">API Web Services</h5>
+        </div>
+        <div className="tooltip" style={skillStyle}>
+          <img src={Postman} alt="postman" id="postman" style={skillStyle} style={{ maxWidth: "100%" }} />
+          <h5 className="tooltiptext">Postman</h5>
+        </div>
+        <br></br>
+        <a href="https://drive.google.com/open?id=1yzKE-yOWZH_VpB7CWBp5ag8tXpFvQWf2" target="_blank" rel="noopener noreferrer">
+          <Button pill theme="dark">Certificates</Button>
+          {/* style={{ minWidth:"30%", maxWidth:"60%", margin: "50px 0", padding: "20px 50px", fontSize:"20px"}} */}
+        </a> 
       </section>
+      
       <section id="contact" className="container">
         <h3 id="h3">Contact</h3>
         <div className="rowAlign">
@@ -213,11 +368,8 @@ const whiteHeading = {
 }
 
 
-const skillstyle = {
+const skillStyle = {
   width: "20%",
   maxWidth: "120px",
-  margin: "1%",
+  margin: "2%",
 }
-
-
-
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,11 +2,13 @@ import "shards-ui/dist/css/shards.min.css";
 import {
     Card,
     CardTitle,
+    CardSubtitle,
     CardImg,
     CardBody,
     CardFooter,
     Button,
-    Badge
+    Badge,
+    CardHeader
 } from "shards-react";
 
 import React, { Component } from 'react'
@@ -43,6 +45,25 @@ class TechnologyBadges extends Component {
     render() {
         return (
             this.props.technologies.map((technology, i) => <Badge key={i} pill theme="secondary" style={{ margin: "5px" }}>{technology}</Badge>)
+        );
+    }
+}
+
+export class WorkCard extends Component {
+    render() {
+        return (
+            <Card style={{ maxWidth: "100%", minWidth: "300px", margin: "50px 0"}}>
+                    <CardHeader>
+                        <div style={{height:"10px"}}></div>
+                        <CardTitle>{this.props.title}</CardTitle>
+                        <div style={{height:"10px"}}></div>
+                        <CardSubtitle>{this.props.subtitle}</CardSubtitle>
+                    </CardHeader>
+                    <CardBody>
+                        <p style={{fontSize:"18px"}}>{this.props.description}</p>
+                        <p style={{fontStyle:"italic"}}>{this.props.technologies}</p>
+                    </CardBody>
+                </Card>
         );
     }
 }
